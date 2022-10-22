e86b77a6e6aca3985a7ad3da58052e85af6678de
Raviteja <ravitejadarvemula@gmail.com>
2022-10-22 17:17:00 +0530

    excersice2

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,93 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import logo2 from "./images/react-logo.png";
-
-function Header() {
+import logo2 from "./images/react-icon-small.png";
+//Excerise -1
+// function Header() {
+//   return (
+//     <header class="main">
+//       <nav className="nav">
+//         <img className="nav-logo" src={logo2} alt="" />
+//         <ul className="nav-items">
+//           <li>Home </li>
+//           <li>About</li>
+//           <li>Contact</li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+// function MainContent() {
+//   return (
+//     <div>
+//       <h1>Reasons I'm excited to learn React</h1>
+//       <ul>
+//         <li>
+//           <b>I like front end development</b>
+//         </li>
+//         <li>
+//           <b>I like to design things</b>
+//         </li>
+//         <li>
+//           <b>React is after JavaScript</b>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+// function Footer() {
+//   return (
+//     <small className="Footer">
+//       © 2021 sunshine development. All rights reserved.
+//     </small>
+//   );
+// }
+// function App() {
+//   return (
+//     <div>
+//       {" "}
+//       <Header /> <MainContent /> <Footer />
+//     </div>
+//   );
+// }
+function Navbar() {
   return (
-    <header class="main">
-      <nav className="nav">
-        <img className="nav-logo" src={logo2} alt="" />
-        <ul className="nav-items">
-          <li>Home </li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
+    <nav>
+      <img src={logo2} className="nav--logo" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React course-Project 1</h4>
+    </nav>
   );
 }
-function MainContent() {
+function Main() {
   return (
-    <div>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ul>
+    <main>
+      <h1 className="main-title">Fun facts about React</h1>
+      <ul className="main-list">
         <li>
-          <b>I like front end development</b>
+          <span> Was first released in 2013</span>
         </li>
         <li>
-          <b>I like to design things</b>
+          <span>Was originally created by Jordan Walke</span>
         </li>
         <li>
-          <b>React is after JavaScript</b>
+          <span>Has well over 100k stars on GitHub</span>
+        </li>
+        <li>
+          <span>Is maintained by Facebook</span>
+        </li>
+        <li>
+          <span>Powers thousands of enterprise apps,including mobile apps</span>
         </li>
       </ul>
-    </div>
-  );
-}
-function Footer() {
-  return (
-    <small className="Footer">
-      © 2021 sunshine development. All rights reserved.
-    </small>
+    </main>
   );
 }
 function App() {
   return (
     <div>
-      {" "}
-      <Header /> <MainContent /> <Footer />
+      <Navbar />;
+      <Main />
     </div>
   );
 }
